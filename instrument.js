7912445353a23ee3568028e4e93be120c8458742
Joey Baker <joey@byjoeybaker.com>
2015-03-04 19:41:24 -0800

    Fail the tests on uncaught exception
    Fixes #31

--- instrument.js
+++ instrument.js
@@ -109,6 +109,10 @@ function logError(src, error, position) {
   }
   var errorLine = '' + lines[position.line - 1]
   var spaces = ''
+
+  // write in tap format so that test fail when there's an uncaught exception
+  write('error', ['not ok - ' + error.message])
+
   write('error', [position.source + ':' +position.line])
   for (var i = 0; i < position.column; i++) {
     spaces += ' '
